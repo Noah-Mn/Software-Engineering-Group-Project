d261b86125209878b3f14ab2cef58752f4159d0c
EMMANUEL-MANGULENJE <58146328+EMMANUEL-MANGULENJE@users.noreply.github.com>
2019-12-29 12:18:56 -0800

    BED/COM/22/17

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./componets/Navbar";
 
-//import './login';
-// import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 class App extends Component {
   render() {
